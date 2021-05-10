1d0f4a76338393d6bfb82721d33b2faae1076378
JuanDGomezM <30983763+JuanDGomezM@users.noreply.github.com>
2021-05-11 01:41:37 +0200

    Añadido 2 Player

--- main.ts
+++ main.ts
@@ -1,26 +1,34 @@
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    steve.setPosition(60, 100)
-})
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+namespace SpriteKind {
+    export const player2 = SpriteKind.create()
+}
+controller.player1.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
     steve.setPosition(30, 100)
-})
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    steve.setPosition(130, 100)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
     otherSprite.destroy(effects.disintegrate, 100)
-    music.setVolume(10)
-    info.changeScoreBy(1)
-})
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    steve.setPosition(100, 100)
+    if (sprite == steve) {
+        info.player1.changeScoreBy(1)
+    }
 })
 info.onLifeZero(function () {
     game.over(true, effects.confetti)
 })
+controller.player1.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    steve.setPosition(70, 100)
+})
 scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
     sprite.destroy(effects.fire, 100)
-    info.changeLifeBy(-1)
+    info.player1.changeLifeBy(-1)
+    scene.cameraShake(4, 500)
+})
+info.player1.onLifeZero(function () {
+    steve.destroy(effects.disintegrate, 500)
+})
+controller.player1.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
+    steve.setPosition(10, 100)
+})
+controller.player1.onButtonEvent(ControllerButton.Down, ControllerButtonEvent.Pressed, function () {
+    steve.setPosition(50, 100)
 })
 let right: Sprite = null
 let down: Sprite = null
@@ -49,9 +57,30 @@ steve = sprites.create(img`
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
-steve.setPosition(80, 100)
-info.setScore(0)
-info.setLife(5)
+steve.setPosition(30, 100)
+let Aurora = sprites.create(img`
+    . . . . . f f f f . . . . . 
+    . . . f f 5 5 5 5 f f . . . 
+    . . f 5 5 5 5 5 5 5 5 f . . 
+    . f 5 5 5 5 5 5 5 5 5 5 f . 
+    . f 5 5 5 d b b d 5 5 5 f . 
+    f 5 5 5 b 4 4 4 4 b 5 5 5 f 
+    f 5 5 c c 4 4 4 4 c c 5 5 f 
+    f b b f b f 4 4 f b f b b f 
+    f b b 4 1 f d d f 1 4 b b f 
+    . f b f d d d d d d f b f . 
+    . f e f e 4 4 4 4 e f e f . 
+    . e 4 f 6 9 9 9 9 6 f 4 e . 
+    . 4 d c 9 9 9 9 9 9 c d 4 . 
+    . 4 f b 3 b 3 b 3 b b f 4 . 
+    . . f f 3 b 3 b 3 3 f f . . 
+    . . . . f f b b f f . . . . 
+    `, SpriteKind.player2)
+Aurora.setPosition(110, 100)
+info.player1.setLife(5)
+info.player1.setScore(0)
+info.player2.setLife(5)
+info.player2.setScore(0)
 let speed = 20
 game.onUpdateInterval(5000, function () {
     speed += 1
@@ -77,7 +106,7 @@ game.onUpdateInterval(4000, function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, SpriteKind.Projectile)
-        left.setPosition(30, 8)
+        left.setPosition(10, 8)
         left.say("IZQUIERDA")
         left.setVelocity(0, speed)
     } else if (lane == 2) {
@@ -99,7 +128,7 @@ game.onUpdateInterval(4000, function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, SpriteKind.Projectile)
-        up.setPosition(60, 8)
+        up.setPosition(30, 8)
         up.say("ARRIBA")
         up.setVelocity(0, speed)
     } else if (lane == 3) {
@@ -121,7 +150,7 @@ game.onUpdateInterval(4000, function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, SpriteKind.Projectile)
-        down.setPosition(100, 8)
+        down.setPosition(50, 8)
         down.say("ABAJO")
         down.setVelocity(0, speed)
     } else {
@@ -143,7 +172,7 @@ game.onUpdateInterval(4000, function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, SpriteKind.Projectile)
-        right.setPosition(130, 8)
+        right.setPosition(70, 8)
         right.say("DERECHA")
         right.setVelocity(0, speed)
     }
