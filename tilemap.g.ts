ad4f748d70b941e3e8c7d2329388d3552d910206
JuanDGomezM <30983763+JuanDGomezM@users.noreply.github.com>
2021-05-11 13:01:02 +0200

    agregado a jugador 2 con sus respectivos controles,vidad,puntos...

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -8,11 +8,15 @@ namespace myTiles {
     export const tile1 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "level":
-            case "level":return tiles.createTilemap(hex`0a0008000202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020201010101010101010101`, img`
+            case "level":return tiles.createTilemap(hex`0a0008000101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010102020202020303030303`, img`
 . . . . . . . . . . 
 . . . . . . . . . . 
 . . . . . . . . . . 
@@ -21,7 +25,7 @@ namespace myTiles {
 . . . . . . . . . . 
 . . . . . . . . . . 
 . . . . . . . . . . 
-`, [myTiles.tile0,myTiles.tile1,myTiles.tile2], TileScale.Sixteen);
+`, [myTiles.tile0,myTiles.tile2,myTiles.tile3,myTiles.tile4], TileScale.Sixteen);
         }
         return null;
     })
@@ -32,6 +36,10 @@ namespace myTiles {
             case "tile0":return tile0;
             case "tile1":return tile1;
             case "tile2":return tile2;
+            case "1player":
+            case "tile3":return tile3;
+            case "2player":
+            case "tile4":return tile4;
         }
         return null;
     })
